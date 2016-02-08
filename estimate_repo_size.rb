3a530817eb6bd118b4168d4f19b0802396a0dd0b
Yvonne Lam <yvonne@chef.io>
2016-02-08 12:19:13 -0800

    Include number of packages per repo.

--- estimate_repo_size.rb
+++ estimate_repo_size.rb
@@ -16,14 +16,15 @@ QUERY
 
   results = Artifactory.post('/api/search/aql', query, 'Content-Type' => 'text/plain')
 
-  size_by_os = Hash.new
+  packages = Hash.new
+  size = Hash.new
   %w[ el ubuntu debian ].each do |os|
     total = 0
-    total = results['results'].find_all { |result| result["path"].include?("\/#{os}\/") }
-            .inject(0) { |sum, result| sum + Integer(result['size']) } 
-    size_by_os[os] = total
+    stuff = results['results'].find_all { |result| result["path"].include?("\/#{os}\/") }
+    packages[os] = stuff.size
+    size[os] = stuff.inject(0) { |sum, result| sum + Integer(result['size']) } 
   end
 
-  puts "#{channel} yum repo size (bytes) = #{size_by_os['el']}"
-  puts "#{channel} apt repo size (bytes) = #{size_by_os['debian'] + size_by_os['ubuntu']}"
+  puts "#{channel} yum repo size (bytes) = #{size['el']} (#{packages['el']} packages)"
+  puts "#{channel} apt repo size (bytes) = #{size['debian'] + size['ubuntu']} (#{packages['debian'] + packages['ubuntu']} packages)"
 end 
